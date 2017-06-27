e8c620f074246b111c0e1433eb2f2ff020a71834
langgoucgq <767947045@qq.com>
2017-06-27 12:34:20 +0800

    选择器测试

--- app/src/main/java/com/example/kyoungcai/espressodemo/MainActivity.java
+++ app/src/main/java/com/example/kyoungcai/espressodemo/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.kyoungcai.espressodemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv= (TextView) findViewById(R.id.tv);
+        imageView= (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"dian",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
--- app/src/test/java/com/example/kyoungcai/espressodemo/Data/LoginDaoImplTest.java
+++ app/src/test/java/com/example/kyoungcai/espressodemo/Data/LoginDaoImplTest.java
@@ -1,10 +1,8 @@
 package com.example.kyoungcai.espressodemo.Data;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
 
 /**
  * Created by KyoungCai on 2017/6/26.
